670271a4f62a57b4ff4f5952cfd3fb0558dc1793
Emil Tullstedt <sakjur@gmail.com>
2018-06-27 00:16:17 +0200

    Fix city flag

--- cmd/sayre/main.go
+++ cmd/sayre/main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/sakjur/sayre/pkg/yr"
 	"flag"
 	"github.com/sakjur/sayre/pkg/yr/cities"
+	"strings"
 )
 
 func main() {
-	city := flag.String("city", "amsterdam", "City to access weather data from")
+	var city string
+	flag.StringVar(&city, "city", "amsterdam", "City to access weather data from")
+	flag.Parse()
+
+	city = strings.ToLower(city)
 
 	citiesMap, err := cities.CitiesToMap()
 	if err != nil {
@@ -15,7 +20,7 @@ func main() {
 		return
 	}
 
-	weatherdata := citiesMap[*city]
+	weatherdata := citiesMap[city]
 
 	for _, c := range weatherdata {
 		data, _ := yr.Fetch(c.Link)
